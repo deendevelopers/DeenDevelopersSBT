099a4c1c5d5f577f773fd16c1cc9742b6c742701
letsgitcracking <45171676+letsgitcracking@users.noreply.github.com>
2022-07-06 23:23:35 +0100

    Polished up natspec

--- contracts/controller/DeenDevelopersSBT.sol
+++ contracts/controller/DeenDevelopersSBT.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-
 /*
 ██████╗░███████╗███████╗███╗░░██╗██████╗░███████╗██╗░░░██╗███████╗██╗░░░░░░█████╗░██████╗░███████╗██████╗░░██████╗
 ██╔══██╗██╔════╝██╔════╝████╗░██║██╔══██╗██╔════╝██║░░░██║██╔════╝██║░░░░░██╔══██╗██╔══██╗██╔════╝██╔══██╗██╔════╝
@@ -17,39 +16,41 @@ import "../domain/tokens/Readable.sol";
 import "../domain/tokens/Burnable.sol";
 import "../domain/accessRoles/AccessRoles.sol";
 
-
 /**
- * @title Deen Developers Hackathon
- * @notice This contract is used to issue tokens for participants of
- * Deen Developer Hackathons. Tokens are SBT (Soul Bound Tokens) which
- * means that they are non-transferrable NFTs. This was decided as being
- * able to transfer your token diminishes its value of proving participation
- * in the Hackathon as participants can sell/transfer their token to a
- * non-participant.
+ * @title Deen Developers NFT Hackathon Contract
  *
- * This token also implements GDPR compliance for SBTs that have metadata with
- * holders personal information. This is acheived by hosting SBT metadata with
- * personal information on infrastructure controlled by the admins. Holders can
- * request for personal information to be removed by contacting the team. All
- * token metadata without personal information are hosted on Arweave+IPFS and
- * there is no way to remove it to maintain the timelessness of the SBTs. In
- * order to give that same persistence and timelessness to SBTs with personal
- * information a switch function has been implemented, which can be triggered
- * by users to switch the personal metadata of the token from the admin
- * controlled infrastructure to the Arweave+IPFS counterpart without personal
- * information. This ensures that in the event that admin infrastructure is
- * unavailable, holders will have a persistent and timelessness SBT. To trigger
- * this switch holders can call switchURI function passing the token ID, this
- * is reversable by calling the function again.
+ * @notice This contract is used to issue tokens for participants of
+ * Deen Developer Hackathons. Tokens are SBT (Soulbound Tokens) which
+ * means that they are non-transferrable. This is because the ability to
+ * transfer your token diminishes the value of the NFT as participants 
+ * can sell/transfer their token to a non-participant.
+ *
+ * By default this contract publishes non-PII data and will only store 
+ * PII with the users explicit consent.
+ *
+ * This contract aims to be GDPR compliant by hosting token metadata 
+ * with PII on centralized infrastructure i.e. controlled by the team. 
+ * Holders can make a request to remove their PII by contacting us.
+ * 
+ * All tokens with non-PII metadata are hosted permanently on Arweave+IPFS. 
+ * In order to give holders the choice of permanance they can execute the 
+ * switch function which host non identifiable data on decentralized 
+ * infrastructure if applicable.
  *
  * @dev SBT is implemented by overriding the transfer and approve functions
- * of OpenZeppelin ERC-721 contract. Non-personalisedToken metadata is permanently
- * stored on Areweave tagging the metadata's CID onto the tag name IPFS-Add. An
- * IPFS extension exists on Arweave nodes to pin files tagged with IPFS-Add onto
- * IPFS. In the event that files get untagged, they can be found by querying IPFS-Add
+ * of OpenZeppelin's ERC-721 contract. Non-personalized token metadata is permanently
+ * stored on Arweave tagging the metadata's CID onto the tag "IPFS-Add". An
+ * IPFS extension exists on Arweave nodes to pin files tagged with "IPFS-Add" on
+ * IPFS. In the event that files get unpinned, they can be found by querying "IPFS-Add"
  * tag on Arweave with the CID value. The file can then be recovered and re-pinned to
  * IPFS.
+ *
+ * @author Russell Choudhury (https://github.com/TheKnightCoder)
+ * @author Sparx (https://github.com/letsgitcracking)
+ *
+ * @custom:contact team@deendevelopers.com
  */
+
 contract DeenDevelopersSBT is
     Dependencies,
     AccessRoles,
@@ -88,9 +89,10 @@ contract DeenDevelopersSBT is
     /**
      * @param tokenId The Id of the token
      * @notice Will return the metadata URI given a token ID. The metadata
-     * will either return personalised metadata hosted on admins infrastructure
-     * or non-personalised metadata hosted on Arweave+IPFS. This can be switched
+     * will either return personalized metadata hosted on admins infrastructure
+     * or non-personalized metadata hosted on Arweave+IPFS. This can be switched
      * by token holders by calling the switchURI function.
+	 *
      * @dev tokenURI switching is determined by the _isHttpURI mapping which
      * maps tokenIds to a boolean, if true the _baseHttpURI concatenated with the
      * tokenId is returned, if false the _tokenURIs mapping is returned.
@@ -106,8 +108,8 @@ contract DeenDevelopersSBT is
 
     /**
      * @param tokenId The Id of the token
-     * @notice Will switch the token metadata to personalised metadata hosted on
-     * admins infrastructure to non-personalised metadata hosted on Arweave+IPFS.
+     * @notice Will switch the token metadata to personalized metadata hosted on
+     * admins infrastructure to non-personalized metadata hosted on Arweave+IPFS.
      * And vice versa.
      * @dev tokenURI switching is determined by the _isHttpURI mapping which
      * maps tokenIds to a boolean.
@@ -119,7 +121,7 @@ contract DeenDevelopersSBT is
     /**
      * @param baseURI the base URI for switched tokens to _isHttpURI true
      * @notice This can only be called by multi-sig admins to change the baseURI
-     * used to host personalised SBTs pointint to admin controlled infrastructure.
+     * used to host personalized SBTs pointing to admin controlled infrastructure.
      */
     function updateBaseURI(string memory baseURI)
         external
@@ -131,9 +133,9 @@ contract DeenDevelopersSBT is
 
     /**
      * @param to The address where the token will be sent to
-     * @param uri The uri link to the metadata of the token
-     * @notice Mint SBT to an Address, minting restricted to authorized accounts
-     * controlled by multi-sig admins
+     * @param uri The URI link to the metadata of the token
+     * @notice Mint SBT to an address, minting restricted to authorized accounts
+     * controlled by multi-sig contract
      */
     function safeMint(address to, string memory uri)
         external
@@ -144,8 +146,8 @@ contract DeenDevelopersSBT is
 
     /**
      * @param tokenId The Id of the token you want to burn
-     * @notice Burn any token, burning restricted to the multi-sig admins, to burn a token you own see BurnMyToken
-     * @dev burning transfers the token to the burn address 0x00..00 and deletes the URI from state var
+     * @notice Burn any token, burning restricted to the multi-sig contract, to burn a token you own; refer to BurnMyToken
+     * @dev burn transfers the token to the burn address 0x00..00 and deletes the URI from storage
      */
     function burn(uint256 tokenId) external onlyRole(DEFAULT_ADMIN_ROLE) {
         Burnable._burnToken(tokenId);
@@ -154,7 +156,7 @@ contract DeenDevelopersSBT is
     /**
      * @param tokenId The Id of the token you want to burn
      * @notice Burn a token you own, caller must pass the ID of the token
-     * @dev burning transfers the token to the burn address 0x00..00 and deletes the URI from state var
+     * @dev burning transfers the token to the burn address 0x00..00 and deletes the URI from storage
      */
     function burnMyToken(uint256 tokenId)
         external
@@ -165,14 +167,14 @@ contract DeenDevelopersSBT is
     }
 
     /**
-     * @notice Pauses burnMyToken, restricted to multi-sig admins
+     * @notice Pauses burnMyToken, restricted to multi-sig contract
      */
     function pauseBurnMyToken() external onlyRole(DEFAULT_ADMIN_ROLE) {
         PausableUpgradeable._pause();
     }
 
     /**
-     * @notice Unpauses burnMyToken, restricted to multi-sig admins
+     * @notice Unpauses burnMyToken, restricted to multi-sig contract
      */
     function unpauseBurnMyToken() external onlyRole(DEFAULT_ADMIN_ROLE) {
         PausableUpgradeable._unpause();
